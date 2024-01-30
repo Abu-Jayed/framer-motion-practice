db52de71f9282bb5d31c3b14c1ca48e76ad043ad
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-30 22:13:05 +0600

    HeroSection responsive now.

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -5,17 +5,23 @@ const HeroSection = () => {
   return (
     <Container className="h-screen pt-16 grid grid-cols-1 lg:grid-cols-2 place-content-center">
       <div>
-        <h1 className="text-8xl font-bold">
-          <span className="text-gray">Don't worry,</span> <br /> <span>We'll fix it</span>
+        <h1 className="text-5xl lg:text-8xl font-bold text-nowrap">
+          <span className="text-gray">Don't worry,</span> <br />{" "}
+          <span>We'll fix it</span>
         </h1>
-        <p>
-          Welcome to <span>iRepair</span>, your one-stop place for all kinds of
-          <span>Macbook repairs</span>
+        <p className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
+          Welcome to{" "}
+          <span className="text-primary-foreground font-semibold">iRepair</span>
+          , your one-stop place for all kinds of
+          <span className="text-primary-foreground font-semibold">
+            {" "}
+            Macbook repairs
+          </span>{" "}
           and diagnostics.
         </p>
       </div>
-      <div>
-        <img src={macbook} alt="" />
+      <div className="mt-10 lg:w-full w-3/4 mx-auto">
+        <img className="-rotate-[35deg] h-[95%] obj " src={macbook} alt="" />
       </div>
     </Container>
   );
