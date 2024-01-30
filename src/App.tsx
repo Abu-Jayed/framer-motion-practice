fe84fdd3b448dce67cefc962c7ca009e4400ec32
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-30 17:26:30 +0600

    react router dom file structure done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useState } from "react";
+import "./App.css";
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div>Hello</div>
-    <Button>Hi</Button>
+      <MainLayout></MainLayout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
