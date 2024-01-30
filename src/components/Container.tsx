be699e22ae01c68b0aa9b45348e87e15c0a6da75
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-30 20:24:26 +0600

    default style and font family configured.

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,6 @@
-const Container = ({children}) => {
+const Container = ({ children }) => {
   return (
-    <div className="w-full h-full max-w-screen-xl px-5 mx-auto bg-rose-800">{children}</div>
+    <div className="w-full h-full max-w-screen-xl px-5 mx-auto">{children}</div>
   );
 };
 
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -4,14 +4,12 @@ import { Button } from "../ui/button";
 const Navbar = () => {
   return (
     <header className="h-16">
-      <nav className="w-full h-full max-w-screen-xl px-5 mx-auto bg-rose-800 flex items-center justify-between">
+      <nav className="w-full h-full max-w-screen-xl px-5 mx-auto  flex items-center justify-between">
         <h1 className="text-3xl">iRepair</h1>
         <ul className="space-x-5">
           <Link to={"/"}>Home</Link>
           <Link to={"/about"}>About</Link>
-          <Button>
-            <Link to={"/login"}>Login</Link>
-          </Button>
+          <Button>Login</Button>
         </ul>
       </nav>
     </header>
