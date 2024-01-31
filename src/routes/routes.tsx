1c1401c071afb893ac01b4dda37def739be614aa
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-31 08:32:09 +0600

    admin layout structure done.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,7 @@
 import App from "@/App";
+import AdminLayout from "@/components/layouts/AdminLayout";
 import About from "@/pages/About";
+import Dashboard from "@/pages/admin/Dashboard";
 import Home from "@/pages/home";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -10,6 +12,15 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home></Home> },
       { path: "/about", element: <About></About> },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout></AdminLayout>,
+    children: [
+      { path: "dashboard", element: <Dashboard></Dashboard> },
+      { path: "service-list", element: <About></About> },
+      { path: "add-service", element: <About></About> },
     ],
   },
 ]);
