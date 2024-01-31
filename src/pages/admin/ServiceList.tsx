ef56d5fbae09d9be851ebe77bf063d78a1b0c799
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-31 10:28:15 +0600

    tanstack query implemented.

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,15 +1,28 @@
+import { getServices } from "@/api/admin/service/service.api";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const ServiceList = () => {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  console.log("service list", data);
+  // console.log("service list", data);
+  // const getData = async() => {}
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/v1/services")
-      .then((res) => res.json())
-      .then((data) => setData(data.data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/api/v1/services")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.data));
+  // }, []);
+
+  
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["services"],
+    queryFn: getServices
+  });
+  console.log(data);
 
   return <div>This is service list</div>;
 };
