82803f43183f785bb4563df5f78312c7d7e6b353
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-31 16:22:36 +0600

    data table created.

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,5 +1,18 @@
 import { getServices } from "@/api/admin/service/service.api";
+import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
 
 const ServiceList = () => {
   const { data, isLoading, isError } = useQuery({
@@ -7,11 +20,15 @@ const ServiceList = () => {
     queryFn: getServices,
   });
 
-
   if (isLoading) {
     return <h1 className="text-rose-800 text-center mt-6">Loading...</h1>;
   }
-  console.log(data?.data.data);
+  const services = data?.data.data.map((item) => ({
+    id: item._id,
+    name: item.name,
+    description: item.description,
+    price: item.price,
+  }));
 
   if (isError) {
     return (
@@ -20,14 +37,42 @@ const ServiceList = () => {
       </h1>
     );
   }
-  
-    return (
-      <div>
-        {data?.data?.data.map((service) => {
-          return <h1 key={service._id}>{service.name}</h1>;
-        })}
-      </div>
-    );
+
+  // {data?.data?.data.map((service) => {
+  //   return <h1 key={service._id}>{service.name}</h1>;
+  // })}
+  return (
+    <Container className="pt-14 rounded-2xl">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {services.map((service) => (
+            <TableRow key={service.id}>
+              <TableCell className="font-medium">{service.name}</TableCell>
+              <TableCell>{service.description}</TableCell>
+              <TableCell>{service.price}</TableCell>
+              <TableCell className="text-right">
+                <Button className="p-2" variant={"destructive"}><Trash2 /></Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total Services</TableCell>
+            <TableCell className="text-right">{services.length}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </Container>
+  );
 };
 
 export default ServiceList;
